9611a6e06d05aa635a1deb5b23ec6d1878519bbf
Caio <caiopadilha@id.uff.br>
2022-09-13 15:52:55 -0300

    feat: model createProduct

--- src/models/products.model.ts
+++ src/models/products.model.ts
@@ -1,10 +1,23 @@
+import { ResultSetHeader } from 'mysql2';
 import connection from './connection';
-import { IProduct, ProductWithRow } from '../interfaces/product.interface';
+import { IProductBody, IProduct, ProductWithRow } from '../interfaces/product.interface';
 
 const productModel = {
   getAll: async (): Promise<IProduct[]> => {
-    const [resul] = await connection.execute<ProductWithRow[]>('SELECT * FROM Trybesmith.Products');
-    return resul;
+    const [result] = await connection
+      .execute<ProductWithRow[]>('SELECT * FROM Trybesmith.Products');
+    return result;
+  },
+  
+  create: async (product: IProductBody): Promise<IProduct> => {
+    const query = 'INSERT INTO Trybesmith.Products (name, amount) VALUES (?, ?)';
+    const [result] = await connection
+      .execute<ResultSetHeader>(query, [product.name, product.amount]);
+    return {
+      id: result.insertId,
+      name: product.name,
+      amount: product.amount,
+    };
   },
 };
 
