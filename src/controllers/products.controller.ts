5f994c91c99fac3aa89199174b31ce86315db2a3
Caio <caiopadilha@id.uff.br>
2022-09-13 15:53:35 -0300

    feat: controller createProduct

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -1,10 +1,19 @@
 import { Request, Response } from 'express';
+import { IProduct, IProductBody } from '../interfaces/product.interface';
 import ProductService from '../services/products.service';
 
 const ProductController = {
   getAll: async (_req: Request, res: Response) => {
     const result = await ProductService.getAll();
     return res.status(200).json(result);
+  },
+
+  create: async (req: Request<unknown, unknown, IProductBody>, res: Response<IProduct>) => {
+    const { body } = req;
+
+    const product = await ProductService.create(body);
+
+    return res.status(201).json(product);
   },
 };
 
