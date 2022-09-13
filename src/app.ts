d55274b62f5ba77c6724ff14c9efc505e4f7dd9a
Caio <caiopadilha@id.uff.br>
2022-09-13 17:17:33 -0300

    feat: endpoint POST createUser /users

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import ProductController from './controllers/products.controller';
+import UserController from './controllers/users.controller';
 
 const app = express();
 
@@ -7,5 +8,6 @@ app.use(express.json());
 
 app.get('/products', ProductController.getAll);
 app.post('/products', ProductController.create);
+app.post('/users', UserController.create);
 
 export default app;
