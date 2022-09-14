e1491d7dc3bc5834c7edd2ac4535033e7bdf45e6
Caio <caiopadilha@id.uff.br>
2022-09-14 20:15:21 -0300

    validations req06 products, productName and productAmount

--- src/app.ts
+++ src/app.ts
@@ -4,13 +4,15 @@ import UserController from './controllers/users.controller';
 import OrderController from './controllers/orders.controller';
 import userBody from './middlewares/validationsUser';
 import LoginController from './controllers/login.controller';
+import validateProductName from './middlewares/validateProductName';
+import validateProductAmount from './middlewares/validateProductAmount';
 
 const app = express();
 
 app.use(express.json());
 
 app.get('/products', ProductController.getAll);
-app.post('/products', ProductController.create);
+app.post('/products', validateProductName, validateProductAmount, ProductController.create);
 app.post('/users', UserController.create);
 app.get('/orders', OrderController.getAll);
 app.post('/login', userBody, LoginController.login);
