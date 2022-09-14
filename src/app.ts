171a133c357cb29ad84d4bf85c59296e9597897e
Caio <caiopadilha@id.uff.br>
2022-09-14 20:44:53 -0300

    validations classe, level, password and username

--- src/app.ts
+++ src/app.ts
@@ -6,15 +6,35 @@ import userBody from './middlewares/validationsUser';
 import LoginController from './controllers/login.controller';
 import validateProductName from './middlewares/validateProductName';
 import validateProductAmount from './middlewares/validateProductAmount';
+import validateUsername from './middlewares/validateUsername';
+import validateClasse from './middlewares/validateClasse';
+import validateLevel from './middlewares/validateLevel';
+import validatePassword from './middlewares/validatePassword';
 
 const app = express();
 
 app.use(express.json());
 
 app.get('/products', ProductController.getAll);
-app.post('/products', validateProductName, validateProductAmount, ProductController.create);
-app.post('/users', UserController.create);
+
+app.post(
+  '/products',
+  validateProductName,
+  validateProductAmount,
+  ProductController.create,
+);
+
+app.post(
+  '/users',
+  validateUsername,
+  validateClasse,
+  validateLevel,
+  validatePassword,
+  UserController.create,
+);
+
 app.get('/orders', OrderController.getAll);
+
 app.post('/login', userBody, LoginController.login);
 
 export default app;
